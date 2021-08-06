cf8038e2d66a507c94a40c3ccce321bbdb58714b
twizzlard <76930650+twizzlard@users.noreply.github.com>
2021-08-05 20:42:29 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,7 +17,7 @@ df = pd.DataFrame({
 df
 
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [50, 50] + [73.76, 122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
